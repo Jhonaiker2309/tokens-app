05ef7800aa9d7de5a1e7812fb8dfd45875e381ae
Jhonaiker Blanco <blancojhonaiker@gmail.com>
2022-02-24 17:34:20 -0500

    almost

---
+++ contracts/Token721.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token721 is ERC721Enumerable ,Ownable {
+  using Strings for uint256;
+
+  string public baseURI;
+  string public baseExtension = ".json";
+  string public notRevealedUri;
+  uint256 public cost = 0.1 ether;
+  uint256 public maxAmountOfNfts = 1000;
+  uint256 public maxAmountOfNftsByUser = 10;
+  bool public paused = true;
+  bool public revealed;
+  mapping(address => bool) userInWhiteList;
+  mapping(address => uint256) public addressMintedBalance;
+  mapping(uint => address) public ownerByToken;
+  mapping(string => bool) public existingRole;
+  mapping(string => mapping(address => bool)) userHasRole;
+  
+
+  constructor(
+  ) ERC721("Warrior Clan NFT", "WCN") {
+    setBaseURI("https://ipfs.io/ipfs/QmUuNFzKA2ya3mU8ac2vUJf3ThoqwYB5i24z7t6QNXpveT");
+    setNotRevealedURI("https://ipfs.io/ipfs/QmUuNFzKA2ya3mU8ac2vUJf3ThoqwYB5i24z7t6QNXpveT/unrevealed.json");
+    createRole("ADMIN");
+    createRole("MINTER");
+  }
+
+  function createRole(string memory _role) private {
+     existingRole[_role] = true;
+  }
+
+  function addUserToRole(string memory _role, address _account) public onlyOwner {
+    require(existingRole[_role] == true, "This role does not exist");
+     userHasRole[_role][_account] = true;
+  }
+
+  function removeUserFromRole(string  memory _role, address  _account) public onlyOwner {
+    require(existingRole[_role] == true, "This role does not exist");
+     userHasRole[_role][_account] = false;
+  }
+  
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  
+  function mint(uint256 _mintAmount) public payable {
+    require(!paused || userInWhiteList[msg.sender] || userHasRole["ADMIN"][msg.sender] || owner() == msg.sender, "the contract is paused");
+    uint256 supply = totalSupply();
+    require(userHasRole["ADMIN"][msg.sender] || userInWhiteList[msg.sender] || userHasRole["MINTER"][msg.sender] || owner() == msg.sender, "You are not allowed to mint");
+    require(_mintAmount > 0, "need to mint at least 1 NFT");
+    require(supply + _mintAmount <= maxAmountOfNfts, "You can exceed the max amount of nfts");
+    require(_mintAmount + addressMintedBalance[msg.sender] <= maxAmountOfNftsByUser, "Each user has a max amount of nfts");
+    require(msg.value == _mintAmount * cost, "You have to pay the right price");
+
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      addressMintedBalance[msg.sender]++;
+      _safeMint(msg.sender, supply + i);
+      ownerByToken[supply + i] = msg.sender;
+    }
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    
+    if(revealed == false) {
+        return notRevealedUri;
+    } else {
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, "/",(tokenId % 11).toString(), baseExtension))
+        : "";
+    }    
+  }
+
+  function addUserToWhiteList(address _user) public {
+      require(owner() == msg.sender || userHasRole["ADMIN"][msg.sender], "You are not allowed to do this action"); 
+      userInWhiteList[_user] = true;
+  }
+
+  function removeUserFromWhiteList(address _user) public {
+    require(owner() == msg.sender || userHasRole["ADMIN"][msg.sender], "You are not allowed to do this action");
+    userInWhiteList[_user] = false;
+  }
+
+  function reveal() public {
+      require(owner() == msg.sender || userHasRole["ADMIN"][msg.sender], "You are not allowed to do this action");
+      revealed = true;
+  }
+  
+  function setNftPerAddressLimit(uint256 _limit) public {
+        require(owner() == msg.sender || userHasRole["ADMIN"][msg.sender], "You are not allowed to do this action");
+        maxAmountOfNftsByUser = _limit;
+  }
+  
+  function setCost(uint256 _newCost) public {
+        require(owner() == msg.sender || userHasRole["ADMIN"][msg.sender], "You are not allowed to do this action");
+        cost = _newCost;
+  }
+
+  function setMaxMintAmount(uint256 _newmaxMintAmount) public {
+    require(owner() == msg.sender || userHasRole["ADMIN"][msg.sender], "You are not allowed to do this action");
+    maxAmountOfNfts = _newmaxMintAmount;
+  }
+
+  function setBaseURI(string memory _newBaseURI) private {
+    baseURI = _newBaseURI;
+  }
+  
+  function setNotRevealedURI(string memory _notRevealedURI) private {
+    notRevealedUri = _notRevealedURI;
+  }
+
+  function pause(bool _state) public {
+    require(owner() == msg.sender || userHasRole["ADMIN"][msg.sender], "You are not allowed to do this action");
+    paused = _state;
+  }
+  
+  function initialWhiteListUsers(address[] memory _users) private {
+    uint numberOfUsers = _users.length;
+    for(uint i = 0; i < numberOfUsers; i++){
+      userInWhiteList[_users[i]] = true;
+      userHasRole["MINTER"][_users[i]] = true;
+    }
+  }
+
+    function burn(uint256 tokenId) public virtual {
+        require(ownerByToken[tokenId] == msg.sender, "You are not the owner of this token");
+        _burn(tokenId);
+        ownerByToken[tokenId] = address(0);
+        addressMintedBalance[msg.sender] = addressMintedBalance[msg.sender] - 1;
+    }
+ 
+    function withdraw()  public onlyOwner payable {
+      payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function contractBalance() public view onlyOwner returns(uint) {
+      return address(this).balance;
+}   
+
+}
---
+++ test/Token721.test.js
@@ -0,0 +1,152 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const {toWei} = require("./utils.js")
+
+describe("Token721", function() {
+
+  let Token721, voting, owner, address1, address2, address3, address4, address5;
+
+  beforeEach( async() => {
+    Token721 = await ethers.getContractFactory("Token721");
+    
+    [owner, address1, address2, address3, address4, address5] = await ethers.getSigners()
+    token = await Token721.deploy();
+  })
+
+  describe("check initial states", () => {
+        it("Should set the right initial values", async () => {
+          expect (await token.notRevealedUri()).to.equal("https://ipfs.io/ipfs/QmUuNFzKA2ya3mU8ac2vUJf3ThoqwYB5i24z7t6QNXpveT/unrevealed.json");
+          expect (await token.baseURI()).to.equal("https://ipfs.io/ipfs/QmUuNFzKA2ya3mU8ac2vUJf3ThoqwYB5i24z7t6QNXpveT");
+          expect (await token.existingRole("ADMIN")).to.equal(true);
+          expect (await token.existingRole("MINTER")).to.equal(true);
+    })
+
+        it("Check role related functions", async () => {
+           
+           expect (await token.existingRole("DOCTOR")).to.equal(false)
+           
+            await expect(
+            token.addUserToRole("DOCTOR", address2.address)
+            ).to.be.revertedWith("This role does not exist");
+
+            await expect(
+            token.removeUserFromRole("DOCTOR", address2.address)
+            ).to.be.revertedWith("This role does not exist");
+
+            await expect(
+            token.connect(address2).addUserToWhiteList(address3.address)
+            ).to.be.revertedWith("You are not allowed to do this action");
+
+            await expect(
+            token.connect(address2).removeUserFromWhiteList(address3.address)
+            ).to.be.revertedWith("You are not allowed to do this action");
+
+            await expect(
+            token.connect(address2).reveal()
+            ).to.be.revertedWith("You are not allowed to do this action");
+
+            await expect(
+            token.connect(address2).setNftPerAddressLimit(20)
+            ).to.be.revertedWith("You are not allowed to do this action");
+
+            await expect(
+            token.connect(address2).setCost(100)
+            ).to.be.revertedWith("You are not allowed to do this action");
+
+            await expect(
+            token.connect(address2).setMaxMintAmount(100000)
+            ).to.be.revertedWith("You are not allowed to do this action");
+
+            await expect(
+            token.connect(address2).pause(false)
+            ).to.be.revertedWith("You are not allowed to do this action");                                                                                  
+        })
+
+        it("Check minting", async () => {
+            await expect(
+            token.connect(address2).mint(1, {value: toWei(0.1)})
+            ).to.be.revertedWith("the contract is paused");   
+
+                    await token.pause(false)
+
+        await expect(
+            token.connect(address2).mint(1, {value: toWei(0.1)})
+            ).to.be.revertedWith("You are not allowed to mint");   
+
+        await token.addUserToRole("ADMIN", address2.address)
+
+
+        await expect(
+            token.connect(address2).mint(0, {value: toWei(0.1)})
+            ).to.be.revertedWith("need to mint at least 1 NFT");   
+
+        await expect(
+            token.connect(address2).mint(100000000, {value: toWei(0.1)})
+            ).to.be.revertedWith("You can exceed the max amount of nfts");       
+
+        await expect(
+            token.connect(address2).mint(15, {value: toWei(0.1)})
+        ).to.be.revertedWith("Each user has a max amount of nfts");
+
+        await expect(
+            token.connect(address2).mint(5, {value: toWei(0.1)})
+        ).to.be.revertedWith("You have to pay the right price"); 
+
+        token.connect(address2).mint(5, {value: toWei(0.5)})
+        
+        let numberOfTokensInAddress = await token.addressMintedBalance(address2.address)
+        expect(await token.addressMintedBalance(address2.address)).to.be.equal(5)
+
+        expect(await token.tokenURI(2)).to.be.equal("https://ipfs.io/ipfs/QmUuNFzKA2ya3mU8ac2vUJf3ThoqwYB5i24z7t6QNXpveT/unrevealed.json")
+
+        await token.reveal()
+
+        expect(await token.tokenURI(2)).to.be.equal("https://ipfs.io/ipfs/QmUuNFzKA2ya3mU8ac2vUJf3ThoqwYB5i24z7t6QNXpveT/2.json")
+
+        token.mint(6, {value: toWei(0.6)})
+
+        expect(await token.addressMintedBalance(owner.address)).to.be.equal(6)
+
+        expect(await token.tokenURI(11)).to.be.equal("https://ipfs.io/ipfs/QmUuNFzKA2ya3mU8ac2vUJf3ThoqwYB5i24z7t6QNXpveT/0.json")
+
+        expect(await token.ownerByToken(5)).to.be.equal(address2.address)
+
+        expect(await token.ownerByToken(11)).to.be.equal(owner.address)   
+
+        await token.burn(11)
+
+        await expect(
+            token.tokenURI(11)
+        ).to.be.revertedWith("ERC721Metadata: URI query for nonexistent token"); 
+
+        expect(await token.contractBalance()).to.be.equal(toWei(1.1))
+
+        await token.withdraw()
+
+        expect(await token.contractBalance()).to.be.equal(toWei(0))
+ 
+        })
+
+        it("Check White list", async () => {
+          
+            await expect(
+            token.connect(address2).mint(3, {value: toWei(0.3)})
+            ).to.be.revertedWith("the contract is paused"); 
+
+            await token.addUserToWhiteList(address2.address) 
+
+            await token.connect(address2).mint(3, {value: toWei(0.3)})
+            
+            expect(await token.addressMintedBalance(address2.address)).to.be.equal(3)   
+
+            await token.removeUserFromWhiteList(address2.address) 
+
+                        await expect(
+            token.connect(address2).mint(3, {value: toWei(0.3)})
+            ).to.be.revertedWith("the contract is paused"); 
+            
+        })
+
+  })
+
+})
